6d09c62156491ed450b105360d9f5be3e730ef1d
pyos <pyos100500@gmail.com>
2015-05-20 19:18:17 +0300

    Expose message writer to Python code.

--- python/test.c
+++ python/test.c
@@ -1,6 +1,7 @@
 //! file: ../src/core.c
 //! file: ../src/iovec.c
 //! file: ../src/error.c
+//! file: ../src/write.c
 //! file: ../picohttpparser/picohttpparser.c
 //! include_dir: ..
 //! include_dir: ../include
@@ -10,6 +11,7 @@
 #include <structmember.h>
 #include "core.h"
 #include "error.h"
+#include "write.h"
 
 
 typedef struct
@@ -24,6 +26,14 @@ typedef struct
 } PyCNO;
 
 
+static PyObject * pycno_handle_cno_error(PyCNO *self)
+{
+    // TODO use different exception types
+    return PyErr_Format(PyExc_RuntimeError, "%d: %s (%s:%d)",
+        cno_error(), cno_error_text(), cno_error_file(), cno_error_line());
+}
+
+
 static void pycno_on_message_start(cno_connection_t *conn, PyCNO *self, size_t stream, cno_message_t *msg)
 {
     if (PyErr_Occurred()) {
@@ -31,7 +41,7 @@ static void pycno_on_message_start(cno_connection_t *conn, PyCNO *self, size_t s
     }
 
     if (self->on_message_start) {
-        PyObject *headers = PyDict_New();
+        PyObject *headers = PyList_New(msg->headers_len);
         size_t i;
 
         if (headers == NULL) {
@@ -39,30 +49,16 @@ static void pycno_on_message_start(cno_connection_t *conn, PyCNO *self, size_t s
         }
 
         for (i = 0; i < msg->headers_len; ++i) {
-            PyObject *name  = PyUnicode_FromStringAndSize(msg->headers[i].name.data, msg->headers[i].name.size);
+            PyObject *header = Py_BuildValue("(s#s#)",
+                msg->headers[i].name.data,  msg->headers[i].name.size,
+                msg->headers[i].value.data, msg->headers[i].value.size);
 
-            if (name == NULL) {
+            if (header == NULL) {
                 Py_DECREF(headers);
                 return;
             }
 
-            PyObject *value = PyUnicode_FromStringAndSize(msg->headers[i].value.data, msg->headers[i].value.size);
-
-            if (value == NULL) {
-                Py_DECREF(headers);
-                Py_DECREF(name);
-                return;
-            }
-
-            if (PyDict_SetItem(headers, name, value)) {
-                Py_DECREF(headers);
-                Py_DECREF(value);
-                Py_DECREF(name);
-                return;
-            }
-
-            Py_DECREF(value);
-            Py_DECREF(name);
+            PyList_SET_ITEM(headers, i, header);
         }
 
         PyObject *ret = PyObject_CallFunction(
@@ -166,9 +162,7 @@ static PyObject * pycno_data_received(PyCNO *self, PyObject *args)
     }
 
     if (cno_connection_data_received(self->conn, data, length)) {
-        cno_connection_destroy(self->conn);
-        self->conn = NULL;
-        return PyErr_Format(PyExc_RuntimeError, "%d: %s (%s:%d)", cno_error(), cno_error_text(), cno_error_file(), cno_error_line());
+        return pycno_handle_cno_error(self);
     }
 
     if (PyErr_Occurred()) {
@@ -201,8 +195,137 @@ static PyObject * pycno_connection_lost(PyCNO *self, PyObject *args)
     }
 
     if (self->conn) {
-        cno_connection_destroy(self->conn);
+        if (cno_connection_destroy(self->conn)) {
+            return pycno_handle_cno_error(self);
+        }
+
         self->conn = NULL;
+    }
+
+    if (PyErr_Occurred()) {
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+
+static PyObject * pycno_write_message(PyCNO *self, PyObject *args, PyObject *kwargs)
+{
+    cno_message_t msg = { 0 };
+    PyObject *headers;
+    Py_ssize_t stream;
+
+    if (self->conn->server) {
+        char *kwds[] = { "stream", "minor", "code", "headers", "chunked", NULL };
+        if (!PyArg_ParseTupleAndKeywords(args, kwargs, "niiO|p", kwds, &stream,
+                &msg.minor, &msg.code, &headers, &msg.chunked)) {
+            return NULL;
+        }
+    } else {
+        char *kwds[] = { "stream", "minor", "method", "path", "headers", "chunked", NULL };
+        if (!PyArg_ParseTupleAndKeywords(args, kwargs, "nis#s#O|p", kwds, &stream,
+                &msg.minor, &msg.method.data, &msg.method.size,
+                &msg.path.data, &msg.path.size, &headers, &msg.chunked)) {
+            return NULL;
+        }
+    }
+
+    msg.headers_len = (size_t) PySequence_Size(headers);
+
+    if (msg.headers_len == (size_t) -1) {
+        return PyErr_Format(PyExc_TypeError, "headers must be a sequence of 2-tuples, not %s", Py_TYPE(headers)->tp_name);
+    }
+
+    PyObject *iter = PyObject_GetIter(headers);
+    PyObject *item;
+
+    if (iter == NULL) {
+        return NULL;
+    }
+
+    msg.headers = PyMem_RawMalloc(sizeof(cno_header_t) * msg.headers_len);
+    cno_header_t *header = msg.headers;
+
+    if (msg.headers == NULL) {
+        Py_DECREF(iter);
+        return PyErr_NoMemory();
+    }
+
+    while ((item = PyIter_Next(iter))) {
+        if (!PyArg_ParseTuple(item, "s#s#",
+                &header->name.data,  &header->name.size,
+                &header->value.data, &header->value.size)) {
+            Py_DECREF(item);
+            Py_DECREF(iter);
+            PyMem_RawFree(msg.headers);
+            return NULL;
+        }
+
+        Py_DECREF(item);
+        ++header;
+    }
+
+    Py_DECREF(iter);
+
+    if (PyErr_Occurred()) {
+        PyMem_RawFree(msg.headers);
+        return NULL;
+    }
+
+    if (cno_write_message(self->conn, (size_t) stream, &msg)) {
+        return pycno_handle_cno_error(self);
+    }
+
+    if (PyErr_Occurred()) {
+        return NULL;
+    }
+
+    PyMem_RawFree(msg.headers);
+    Py_RETURN_NONE;
+}
+
+
+static PyObject * pycno_write_data(PyCNO *self, PyObject *args, PyObject *kwargs)
+{
+    Py_ssize_t stream;
+    Py_ssize_t length;
+    const char *data;
+    int chunked = 0;
+    char *kwds[] = { "stream", "data", "chunked", NULL };
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ny#|p", kwds, &stream, &data, &length, &chunked)) {
+        return NULL;
+    }
+
+    if (cno_write_data(self->conn, stream, data, length, chunked)) {
+        return pycno_handle_cno_error(self);
+    }
+
+    if (PyErr_Occurred()) {
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+
+static PyObject * pycno_write_end(PyCNO *self, PyObject *args, PyObject *kwargs)
+{
+    Py_ssize_t stream;
+    int chunked = 0;
+    char *kwds[] = { "stream", "chunked", NULL };
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "n|p", kwds, &stream, &chunked)) {
+        return NULL;
+    }
+
+    if (cno_write_end(self->conn, stream, chunked)) {
+        return pycno_handle_cno_error(self);
+    }
+
+    if (PyErr_Occurred()) {
+        return NULL;
     }
 
     Py_RETURN_NONE;
@@ -211,8 +334,19 @@ static PyObject * pycno_connection_lost(PyCNO *self, PyObject *args)
 
 static void pycno_dealloc(PyCNO *self)
 {
-    cno_connection_destroy(self->conn);
-    self->conn = NULL;
+    if (self->conn) {
+        if (cno_connection_destroy(self->conn)) {
+            pycno_handle_cno_error(self);
+        }
+
+        self->conn = NULL;
+    }
+
+    if (PyErr_Occurred()) {
+        PyErr_WriteUnraisable((PyObject *) self);
+        PyErr_Clear();
+    }
+
     Py_XDECREF(self->transport);
     Py_XDECREF(self->on_write);
     Py_XDECREF(self->on_message_start);
@@ -226,6 +360,9 @@ static PyMethodDef PyCNOMethods[] = {
     { "data_received",   (PyCFunction) pycno_data_received,   METH_VARARGS, NULL },
     { "connection_made", (PyCFunction) pycno_connection_made, METH_VARARGS, NULL },
     { "connection_lost", (PyCFunction) pycno_connection_lost, METH_VARARGS, NULL },
+    { "write_message",   (PyCFunction) pycno_write_message,   METH_VARARGS | METH_KEYWORDS, NULL },
+    { "write_data",      (PyCFunction) pycno_write_data,      METH_VARARGS | METH_KEYWORDS, NULL },
+    { "write_end",       (PyCFunction) pycno_write_end,       METH_VARARGS | METH_KEYWORDS, NULL },
     { NULL }
 };
 
