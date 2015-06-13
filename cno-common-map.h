1cfbd13f0346e185dd3417b264a020d912b565dc
pyos <pyos100500@gmail.com>
2015-06-14 01:09:13 +0300

    Save 4 bytes per bucket by not having a redundant field.

--- cno-common-map.h
+++ cno-common-map.h
@@ -1,25 +1,27 @@
-struct cno_st_map_bucket_t { CNO_LIST_LINK(struct cno_st_map_bucket_t); size_t key; };
+struct cno_st_map_handle_t { CNO_LIST_LINK(struct cno_st_map_handle_t); size_t key; };
+struct cno_st_map_bucket_t { CNO_LIST_LINK(struct cno_st_map_handle_t); };
 
 
-#define CNO_MAP(size) struct { struct cno_st_map_bucket_t __map_root_handle[size]; }
-#define CNO_MAP_VALUE struct { struct cno_st_map_bucket_t __map_item_handle[1]; }
+#define CNO_MAP(size) struct { struct cno_st_map_bucket_t __map_bucket[size]; }
+#define CNO_MAP_VALUE struct { struct cno_st_map_handle_t __map_handle[1]; }
 
 
-#define cno_map_size(m) sizeof((m)->__map_root_handle) / sizeof((m)->__map_root_handle[0])
-#define cno_map_init(m)            __cno_map_init(   cno_map_size(m), (m)->__map_root_handle)
-#define cno_map_insert(m, k, x)    __cno_map_insert( cno_map_size(m), (m)->__map_root_handle, k, (x)->__map_item_handle)
-#define cno_map_find(m, k)         __cno_map_find(   cno_map_size(m), (m)->__map_root_handle, k)
-#define cno_map_remove(m, x)       cno_list_remove((x)->__map_item_handle)
-#define cno_map_clear(m)           cno_map_iterate(m, struct cno_st_map_bucket_t, __x, cno_list_remove(__x))
+#define cno_map_size(m) sizeof((m)->__map_bucket) / sizeof((m)->__map_bucket[0])
+#define cno_map_init(m)            __cno_map_init(   cno_map_size(m), (m)->__map_bucket)
+#define cno_map_insert(m, k, x)    __cno_map_insert( cno_map_size(m), (m)->__map_bucket, k, (x)->__map_handle)
+#define cno_map_find(m, k)         __cno_map_find(   cno_map_size(m), (m)->__map_bucket, k)
+#define cno_map_remove(m, x)       cno_list_remove((x)->__map_handle)
+#define cno_map_clear(m)           cno_map_iterate(m, struct cno_st_map_handle_t, __x, cno_list_remove(__x))
 
 
-#define cno_map_iterate(m, T, var, block) do {                                        \
-    T *var;                                                                           \
-    size_t s = cno_map_size(m);                                                       \
-    struct cno_st_map_bucket_t *__n, *__m, *__i;                                      \
-    for (__m = (m)->__map_root_handle; s--; ++__m)                                    \
-    for (__i = __m->next, __n = __i->next; __i != __m; __i = __n, __n = __i->next)    \
-    { var = (T *) __i; block; }                                                       \
+#define cno_map_iterate(m, T, var, block) do {                                              \
+    T *var;                                                                                 \
+    size_t s = cno_map_size(m);                                                             \
+    struct cno_st_map_bucket_t *__m;                                                        \
+    struct cno_st_map_handle_t *__n, *__i;                                                  \
+    for (__m = (m)->__map_bucket; s--; ++__m)                                               \
+    for (__i = __m->next, __n = __i->next; __i != (void *) __m; __i = __n, __n = __i->next) \
+    { var = (T *) __i; block; }                                                             \
 } while (0)
 
 
@@ -35,7 +37,7 @@ static inline size_t __cno_map_hash(size_t key, size_t size)
 }
 
 
-static inline void __cno_map_insert(size_t size, struct cno_st_map_bucket_t *map, size_t key, struct cno_st_map_bucket_t *ob)
+static inline void __cno_map_insert(size_t size, struct cno_st_map_bucket_t *map, size_t key, struct cno_st_map_handle_t *ob)
 {
     cno_list_insert_after(map + __cno_map_hash(ob->key = key, size), ob);
 }
@@ -44,12 +46,10 @@ static inline void __cno_map_insert(size_t size, struct cno_st_map_bucket_t *map
 static void *__cno_map_find(size_t size, struct cno_st_map_bucket_t *map, size_t key)
 {
     struct cno_st_map_bucket_t *root = map + __cno_map_hash(key, size);
-    struct cno_st_map_bucket_t *it   = root->next;
+    struct cno_st_map_handle_t *it   = root->next;
 
-    for (; it != root; it = it->next) {
-        if (key == it->key) {
-            return it;
-        }
+    for (; it != (struct cno_st_map_handle_t *) root; it = it->next) if (key == it->key) {
+        return it;
     }
 
     return NULL;
