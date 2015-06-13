13aa1cbdb731b274ad171ea077d0235befa0e5e7
pyos <pyos100500@gmail.com>
2015-06-14 00:45:06 +0300

    Move stuff around, use stack more.

--- cno.c
+++ cno.c
@@ -498,18 +498,13 @@ static int cno_frame_handle(cno_connection_t *conn, cno_frame_t *frame)
     if (stream && frame->flags & CNO_FLAG_END_HEADERS) {
         stream->last_frame = 0;
 
-        size_t limit = 256;
-        cno_header_t *headers = malloc(sizeof(cno_header_t) * limit);
+        size_t limit = CNO_MAX_HEADERS;
+        cno_header_t  headers[CNO_MAX_HEADERS];
         cno_header_t *it;
-
-        if (headers == NULL) {
-            return CNO_ERROR_NO_MEMORY;
-        }
 
         if (cno_hpack_decode(&conn->decoder, &stream->cache, headers, &limit)) {
             (void) cno_frame_write_goaway(conn, CNO_STATE_COMPRESSION_ERROR);
             cno_io_vector_clear(&stream->cache);
-            free(headers);
             return CNO_PROPAGATE;
         }
 
@@ -551,8 +546,6 @@ static int cno_frame_handle(cno_connection_t *conn, cno_frame_t *frame)
             cno_io_vector_clear(&it->name);
             cno_io_vector_clear(&it->value);
         }
-
-        free(headers);
 
         if (failed) {
             return CNO_PROPAGATE;
@@ -731,8 +724,8 @@ int cno_connection_made(cno_connection_t *conn)
                 }
             }
 
-            size_t header_num = CNO_MAX_HTTP1_HEADERS;
-            struct phr_header headers[CNO_MAX_HTTP1_HEADERS], *it = headers, *end;
+            size_t header_num = CNO_MAX_HEADERS;
+            struct phr_header headers[CNO_MAX_HEADERS], *it = headers, *end;
 
             int minor;
             int ok = conn->client
@@ -1044,6 +1037,59 @@ static inline int cno_finalize_http2(cno_connection_t *conn, cno_stream_t *strea
     }
 
     return CNO_OK;
+}
+
+
+static inline const char *cno_message_literal(const struct cno_st_message_t *msg)
+{
+    switch (msg->code) {
+        case 100: return "Continue";
+        case 101: return "Switching Protocols";
+        case 200: return "OK";
+        case 201: return "Created";
+        case 202: return "Accepted";
+        case 203: return "Non-Authoritative Information";
+        case 204: return "No Content";
+        case 205: return "Reset Content";
+        case 206: return "Partial Content";
+        case 300: return "Multiple Choices";
+        case 301: return "Moved Permanently";
+        case 302: return "Found";
+        case 303: return "See Other";
+        case 304: return "Not Modified";
+        case 305: return "Use Proxy";
+        case 306: return "(Unused)";
+        case 307: return "Temporary Redirect";
+        case 400: return "Bad Request";
+        case 401: return "Unauthorized";
+        case 402: return "Payment Required";
+        case 403: return "Forbidden";
+        case 404: return "Not Found";
+        case 405: return "Method Not Allowed";
+        case 406: return "Not Acceptable";
+        case 407: return "Proxy Authentication Required";
+        case 408: return "Request Timeout";
+        case 409: return "Conflict";
+        case 410: return "Gone";
+        case 411: return "Length Required";
+        case 412: return "Precondition Failed";
+        case 413: return "Request Entity Too Large";
+        case 414: return "Request-URI Too Long";
+        case 415: return "Unsupported Media Type";
+        case 416: return "Requested Range Not Satisfiable";
+        case 417: return "Expectation Failed";
+        case 428: return "Precondition Required";
+        case 429: return "Too Many Requests";
+        case 431: return "Request Header Fields Too Large";
+        case 500: return "Internal Server Error";
+        case 501: return "Not Implemented";
+        case 502: return "Bad Gateway";
+        case 503: return "Service Unavailable";
+        case 504: return "Gateway Timeout";
+        case 505: return "HTTP Version Not Supported";
+        case 511: return "Network Authentication Required";
+        default:  return "Unknown";
+    }
 }
 
 
--- cno.h
+++ cno.h
@@ -15,9 +15,9 @@
 #endif
 
 
-#ifndef CNO_MAX_HTTP1_HEADERS
+#ifndef CNO_MAX_HEADERS
 // Max. number of entries in the header table of inbound messages.
-#define CNO_MAX_HTTP1_HEADERS 128
+#define CNO_MAX_HEADERS 128
 #endif
 
 
@@ -196,59 +196,6 @@ CNO_STRUCT_EXPORT(message);
 
 extern const char  CNO_FRAME_FLOW_CONTROLLED[256];
 extern const char *CNO_FRAME_NAME[256];
-
-
-static inline const char *cno_message_literal(const struct cno_st_message_t *msg)
-{
-    switch (msg->code) {
-        case 100: return "Continue";
-        case 101: return "Switching Protocols";
-        case 200: return "OK";
-        case 201: return "Created";
-        case 202: return "Accepted";
-        case 203: return "Non-Authoritative Information";
-        case 204: return "No Content";
-        case 205: return "Reset Content";
-        case 206: return "Partial Content";
-        case 300: return "Multiple Choices";
-        case 301: return "Moved Permanently";
-        case 302: return "Found";
-        case 303: return "See Other";
-        case 304: return "Not Modified";
-        case 305: return "Use Proxy";
-        case 306: return "(Unused)";
-        case 307: return "Temporary Redirect";
-        case 400: return "Bad Request";
-        case 401: return "Unauthorized";
-        case 402: return "Payment Required";
-        case 403: return "Forbidden";
-        case 404: return "Not Found";
-        case 405: return "Method Not Allowed";
-        case 406: return "Not Acceptable";
-        case 407: return "Proxy Authentication Required";
-        case 408: return "Request Timeout";
-        case 409: return "Conflict";
-        case 410: return "Gone";
-        case 411: return "Length Required";
-        case 412: return "Precondition Failed";
-        case 413: return "Request Entity Too Large";
-        case 414: return "Request-URI Too Long";
-        case 415: return "Unsupported Media Type";
-        case 416: return "Requested Range Not Satisfiable";
-        case 417: return "Expectation Failed";
-        case 428: return "Precondition Required";
-        case 429: return "Too Many Requests";
-        case 431: return "Request Header Fields Too Large";
-        case 500: return "Internal Server Error";
-        case 501: return "Not Implemented";
-        case 502: return "Bad Gateway";
-        case 503: return "Service Unavailable";
-        case 504: return "Gateway Timeout";
-        case 505: return "HTTP Version Not Supported";
-        case 511: return "Network Authentication Required";
-        default:  return "Unknown";
-    }
-}
 
 
 cno_connection_t * cno_connection_new           (enum CNO_CONNECTION_KIND kind);
