070e2bc6363b8f84d9e0b9fff8d95f88b729a77f
pyos <pyos100500@gmail.com>
2015-06-22 20:39:08 +0300

    Write a fake reason in the HTTP/1.x status line.
    The standard allows this, so why not use this to cut the amount
    of code? It's not like anyone is even reading these. The code
    by itself is sufficient.

--- cno.c
+++ cno.c
@@ -1202,59 +1202,6 @@ int cno_connection_lost(cno_connection_t *conn)
 }
 
 
-static inline const char *cno_message_literal(const struct cno_st_message_t *msg)
-{
-    switch (msg->code) {
-        case 100: return "Continue";
-        case 101: return "Switching Protocols";
-        case 200: return "OK";
-        case 201: return "Created";
-        case 202: return "Accepted";
-        case 203: return "Non-Authoritative Information";
-        case 204: return "No Content";
-        case 205: return "Reset Content";
-        case 206: return "Partial Content";
-        case 300: return "Multiple Choices";
-        case 301: return "Moved Permanently";
-        case 302: return "Found";
-        case 303: return "See Other";
-        case 304: return "Not Modified";
-        case 305: return "Use Proxy";
-        case 306: return "(Unused)";
-        case 307: return "Temporary Redirect";
-        case 400: return "Bad Request";
-        case 401: return "Unauthorized";
-        case 402: return "Payment Required";
-        case 403: return "Forbidden";
-        case 404: return "Not Found";
-        case 405: return "Method Not Allowed";
-        case 406: return "Not Acceptable";
-        case 407: return "Proxy Authentication Required";
-        case 408: return "Request Timeout";
-        case 409: return "Conflict";
-        case 410: return "Gone";
-        case 411: return "Length Required";
-        case 412: return "Precondition Failed";
-        case 413: return "Request Entity Too Large";
-        case 414: return "Request-URI Too Long";
-        case 415: return "Unsupported Media Type";
-        case 416: return "Requested Range Not Satisfiable";
-        case 417: return "Expectation Failed";
-        case 428: return "Precondition Required";
-        case 429: return "Too Many Requests";
-        case 431: return "Request Header Fields Too Large";
-        case 500: return "Internal Server Error";
-        case 501: return "Not Implemented";
-        case 502: return "Bad Gateway";
-        case 503: return "Service Unavailable";
-        case 504: return "Gateway Timeout";
-        case 505: return "HTTP Version Not Supported";
-        case 511: return "Network Authentication Required";
-        default:  return "Unknown";
-    }
-}
-
-
 int cno_write_reset(cno_connection_t *conn, size_t stream)
 {
     return cno_frame_write_rst_stream(conn, stream, CNO_STATE_NO_ERROR);
@@ -1341,7 +1288,7 @@ int cno_write_message(cno_connection_t *conn, size_t stream, const cno_message_t
             ptr = write_vector(ptr, &msg->path);
             ptr = write_string(ptr, " HTTP/1.1\r\n");
         } else {
-            ptr = write_format(ptr, "HTTP/1.1 %d %s\r\n", msg->code, cno_message_literal(msg));
+            ptr = write_format(ptr, "HTTP/1.1 %d No Reason\r\n", msg->code);
         }
 
         for (; it != end; ++it) {
