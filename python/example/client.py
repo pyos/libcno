3d92c13f5436402aa1cdd2318f6c1ddeb130dfd2
dawg <pyos100500@gmail.com>
2017-04-04 10:39:09 +0300

    Show which response is to which request in the example client.

--- python/example/client.py
+++ python/example/client.py
@@ -10,12 +10,13 @@ async def main(url):
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
     resp = await cno.request(loop, 'GET', url, ssl_ctx=ctx)
-    await print_response(resp)
+    await print_response([('GET', url)], resp)
     resp.conn.close()
 
 
-async def print_response(rsp):
-    print('>>> \033[1;3{}m{} (http/{})\033[39m'.format(
+async def print_response(chain, rsp):
+    head = ' \033[1;35m->\033[0m '.join('\033[1;35m{}\033[0m {}'.format(method, url) for method, url in chain)
+    print('>>>', head, '\033[1;3{}m{} (http/{})\033[39m'.format(
         2 if 200 <= rsp.code < 300 else
         1 if 400 <= rsp.code < 600 else 2, rsp.code, 2 if rsp.conn.is_http2 else 1))
     for k, v in rsp.headers:
@@ -28,7 +29,7 @@ async def print_response(rsp):
         print('>>> \033[1;35mpush {}\033[0m {}'.format(push.method, push.path))
         for k, v in push.headers:
             print('    \033[1;34m{}\033[0m: {}'.format(k, v))
-        await print_response((await push.response))
+        await print_response(chain + [(push.method, push.path)], (await push.response))
 
 
 if len(sys.argv) != 2:
